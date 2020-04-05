cbac88b9bcd2100e3d6e795e9a69e877e071c5cc
Ayesha-iqbal <ayesha.sh@ikh.me>
2020-04-04 20:14:35 -0700

    Add files via upload

---
+++ PlagueUpdate/PlaguePanel.java
@@ -0,0 +1,52 @@
+package Plague;
+import javax.swing.*;
+import mvc.*;
+import SimStationpac.*;
+import java.awt.*;
+
+public class PlaguePanel extends SimStationPanel {
+
+    private JButton startButton;
+    private JButton suspendButton;
+    private JButton resumeButton;
+    private JButton stopButton;
+    private JButton statsButton;
+
+    public PlaguePanel(SimStationFactory simStationFactory) {
+
+        super(simStationFactory);
+        SimulationView simulationView = new SimulationView(this.model);
+        this.setLayout(new GridLayout(1, 2));
+
+        startButton = new JButton("Start");
+        suspendButton = new JButton("Suspend");
+        resumeButton = new JButton("Resume");
+        stopButton = new JButton("Stop");
+        statsButton = new JButton("Stats");
+
+        startButton.addActionListener(this);
+        suspendButton.addActionListener(this);
+        resumeButton.addActionListener(this);
+        stopButton.addActionListener(this);
+        statsButton.addActionListener(this);
+
+        JPanel buttonsPanel = new JPanel();
+        buttonsPanel.setLayout(new FlowLayout());
+        buttonsPanel.add(startButton);
+        buttonsPanel.add(suspendButton);
+        buttonsPanel.add(resumeButton);
+        buttonsPanel.add(stopButton);
+        buttonsPanel.add(statsButton);
+        add(buttonsPanel);
+        add(simulationView);
+    }
+
+    public static void main(String[] args) {
+
+        SimStationFactory simStationFactory = new PlagueFactory();
+        SimStationPanel appPanel = new PlaguePanel(simStationFactory);
+        appPanel.display();
+
+    }
+
+}
---
+++ PlagueUpdate/PlagueSimulation.java
@@ -0,0 +1,57 @@
+package Plague;
+import mvc.*;
+import SimStationpac.*;
+public class PlagueSimulation extends Simulation {
+
+    private final static int AGENT_COUNT = 50;
+    public final static int VIRULENCE = 50;
+    public final static int RESISTANCE = 2;
+
+    @Override
+    public String getStats() {
+
+        int infectedCounter = 0;
+        for (Agent agent : this.agents) {
+            if (((RandomWalker) agent).isInfected()) {
+                infectedCounter++;
+            }
+        }
+        double infectedPercentage = ((float) infectedCounter / this.agents.length) * 100;
+
+        return "Number of agents: " + this.agents.length +
+                "\nClock: " + this.clock +
+                "\n% Infected: " + infectedPercentage;
+
+    }
+
+    @Override
+    public Agent getNeighbor(Agent a) {
+
+        for (int i = 0; i < this.agents.length; i++) {
+            if (this.agents[i] == a) {
+                continue;
+            }
+            double distance = Math.sqrt(Math.pow(a.getX() - this.agents[i].getX(), 2) + Math.pow(a.getY() - this.agents[i].getY(), 2));
+            if (distance <= 4) {
+                return this.agents[i];
+            }
+        }
+        return null;
+
+    }
+
+    @Override
+    public void populate() {
+
+        this.agents = new RandomWalker[AGENT_COUNT];
+        for (int i = 0; i < this.agents.length; i++) {
+
+            int infectedChooser = (int) (Math.random() * 100 + 1);
+            boolean infected = (infectedChooser < VIRULENCE) ? true : false;
+            this.agents[i] = new RandomWalker("RW", "NORTH", (int) (Math.random() * WORLD_WIDTH + 1), (int) (Math.random() * WORLD_HEIGHT + 1), this, infected);
+
+        }
+
+    }
+
+}
