b0b697d2949a108b3a758f822cea9eabba3c67ab
Ayesha-iqbal <ayesha.sh@ikh.me>
2020-04-04 20:16:36 -0700

    Add files via upload

---
+++ PlagueUpdate/PlagueFactory.java
@@ -0,0 +1,65 @@
+package Plague;
+import mvc.*;
+import SimStationpac.*;
+public class PlagueFactory extends SimStationFactory {
+
+    @Override
+    public Model makeModel() {
+
+        return new PlagueSimulation();
+
+    }
+
+    @Override
+    public String[] getEditCommands() {
+
+        return new String[]{"Start", "Suspend", "Resume", "Stop", "Stats"};
+
+    }
+
+    @Override
+    public Command makeEditCommand(Model model, String type) {
+
+        if (type == "Start") {
+            return new StartCommand(model);
+        } else if (type == "Suspend") {
+            return new SuspendCommand(model);
+        } else if (type == "Resume") {
+            return new ResumeCommand(model);
+        } else if (type == "Stop") {
+            return new StopCommand(model);
+        } else if (type == "Stats") {
+            return new StatsCommand(model);
+        }
+        return null;
+
+    }
+
+    @Override
+    public String getTitle() {
+
+        return "SimStation";
+
+    }
+
+
+    @Override
+    public String getHelp() {
+
+        return "Click:\n " +
+                "1. Start - start the simulation.\n " +
+                "2. Suspend - pause the simulation.\n " +
+                "3. Resume - resume the simulation.\n " +
+                "4. Stop - stop the simulation.\n " +
+                "5. Stats - show stats.\n";
+
+    }
+
+    @Override
+    public String about() {
+
+        return "SimStation version 1.0.";
+
+    }
+
+}
