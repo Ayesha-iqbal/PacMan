fc432060c74f70dad34857fa96888e3f166d1065
StarMonnP <62403389+StarMonnP@users.noreply.github.com>
2020-04-15 09:32:22 -0700

    Add files via upload

---
+++ Updated15/RandomWalk/RandomWalker.java
@@ -0,0 +1,25 @@
+package RandomWalk;
+import java.awt.*;
+import mvc.*;
+import SimStationpac.*;
+
+public class RandomWalker extends Agent {
+
+    private final static int MAX_STEPS = 10;
+    private final static String[] headings = {"NORTH", "EAST", "SOUTH", "WEST"};
+
+    public RandomWalker(String name, String heading, int x, int y, Simulation world) {
+        super(name, heading, x, y, world);
+        setColor(Color.red);
+    }
+
+    @Override
+    public void update() {
+
+        int steps = (int) (Math.random() * MAX_STEPS + 1);
+        setHeading(headings[(int) (Math.random() * 3 + 1)]);
+        move(steps);
+
+    }
+
+}
